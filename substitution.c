728031cc4827b576fda61682a3b4d5eb956eb6d3
Eric Cheng <68196762+import-brain@users.noreply.github.com>
2021-09-13 09:34:40 -0400

    comments

--- substitution.c
+++ substitution.c
@@ -5,50 +5,50 @@
 #include <math.h>
 #include <ctype.h>
 
-int detectDuplicateCharacters(string input);
+int detectDuplicateCharacters(string input); // function prototype
 
 int main(int argc, string argv[])
 {
-    if (argc != 2)
+    if (argc != 2) // check for two command line arguments, including ./substitution
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
 
-    string key = argv[1];
+    string key = argv[1]; // after computer confirms argv[1] exists, store it in a string
 
-    if (strlen(key) != 26)
+    if (strlen(key) != 26) // if key is not 26 characters, return error
     {
         printf("Key must contain 26 letters\n");
         return 1;
     }
 
     for (int i = 0, n = strlen(key); i < n; i++)
-    {
+    { // for every char in key, if not in alphabet, return error and exit
         if (!isalpha(key[i]))
         {
             printf("Key must contain letters (not numbers, spaces, etc.)\n");
             return 1;
         }
     }
-
+    // call duplicate char checking function
     int duplicateFailure = detectDuplicateCharacters(key);
-
+    // if duplicate chars detected, return error message
     if (duplicateFailure == 1)
     {
         return 1;
     }
-
+    // gets plaintext string from user
     string plaintext = get_string("plaintext: ");
 
 }
 
 int detectDuplicateCharacters(string input)
 {
-    int occurrences[26] = {0};
+    int occurrences[26] = {0}; // array for occurences of each letter
     bool failure = false;
     
-    for (int i = 0, n = strlen(input); i < n; i++)
+    for (int i = 0, n = strlen(input); i < n; i++) // ASCII magic to check for duplications
     {
         if (isupper(input[i]))
         {
